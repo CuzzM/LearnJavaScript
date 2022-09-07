86a3857d2106b1931cb17410313d7db63eb544c5
CuzzM <1315302992@qq.com>
2022-09-07 16:10:08 +0800

    #04 修改倒计时代码

--- built-in-object/Date/2.js
+++ built-in-object/Date/2.js
@@ -5,19 +5,16 @@
 function countDown(plannedTime){
     let nowTimeStamp = + new Date()
     let plannedTimeStamp = +new Date(plannedTime)
-    let second = Math.floor((plannedTimeStamp - nowTimeStamp) / 1000);
-    let k1 = 24 * 60 * 60;
-    let k2 = 60 * 60
-    let k3 = 60
-    let days = parseInt(second / k1)
-    let hours = parseInt((second % k1) / k2)
-    let minutes = parseInt(((second % k1) % k2) / k3)
-    let seconds = parseInt(((second % k1) % k2) % k3)
+    let second = (plannedTimeStamp - nowTimeStamp) / 1000;
+    let days = parseInt(second / 60 / 60 / 24)
+    let hours = parseInt(second / 60 / 60 % 24)
+    let minutes = parseInt(second / 60 % 60)
+    let seconds = parseInt(second % 60)
     console.log(`倒计时${days}天${hours}小时${minutes}分钟${seconds}秒`);
 }
 
 
 setInterval(() => {
-    countDown('2022-9-7 16:00:00');
+    countDown('2022-9-7 16:15:00');
 }, 1000);
 
